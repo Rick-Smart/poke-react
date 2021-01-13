7ba3494358a18d068022b191172139be884f59da
Xis <rick-s@hotmail.com>
2021-01-13 13:36:42 -0800

    pika pika

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 class Detail extends React.Component {
   state = {
@@ -17,8 +19,20 @@ class Detail extends React.Component {
   };
 
   render() {
-    console.log(this.state);
-    return <div>Hey there, I am detail</div>;
+    const { details } = this.state;
+    console.log(details);
+    return (
+      <div className="text-center">
+        <Card style={{ width: "18rem" }} className="text-center">
+          <Card.Img variant="top" src={details.sprites?.front_default} />
+          <Card.Body>
+            <Card.Title>{details.name}</Card.Title>
+            <Card.Text>Weight: {details.weight}</Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </div>
+    );
   }
 }
 
